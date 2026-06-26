486b5af78395f3f1678e29ed26c672206687a0aa
gettking <gettergian@gmail.com>
2026-06-27 06:24:17 +0700

    Update otomatis

--- mulai.py
+++ mulai.py
@@ -1,6 +1,6 @@
 import os
 import time
-from feedback import feedback
+
 
 print()
 print("Sakedap ya nyonyot aku ♥️")
@@ -21,21 +21,4 @@ else:
 
 time.sleep(2)
 
-# MENU setelah update
-while True:
-    print("\n=== GAME MENU ===")
-    print("1. Mulai Game")
-    print("2. Kirim Laporan / Saran")
-    print("3. Keluar")
 
-    pilih = input("\nPilih: ")
-
-    if pilih == "1":
-        os.system("python Tgame.py")
-        break
-
-    elif pilih == "2":
-        feedback()
-
-    elif pilih == "3":
-        break
